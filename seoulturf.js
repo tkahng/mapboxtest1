363a49db6e356a880d81109e563e764e67797768
tkahng <tkahng@gmail.com>
2019-05-11 02:53:42 +0900

    Update seoulturf.js

--- seoulturf.js
+++ seoulturf.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoidGthaG5nIiwiYSI6ImNqOTU3aWtnejRldGgycnF6d3JueG5wb2IifQ.vOYkEc5_mcoA2gtILL5ZmA';
 var map = new mapboxgl.Map({
   container: 'map', // container id
-  style: 'mapbox://styles/tkahng/cjvi9mbok047b1codknhyc2xu', // stylesheet location
+  style: 'mapbox://styles/mapbox/dark-v9', // stylesheet location
   center: [126.994, 37.518], // starting position [lng, lat]
   zoom: 11 // starting zoom
 });
@@ -13,18 +13,15 @@ var hexgrid = turf.hexGrid(bbox, cellSide, options);
 
 map.on('load', function() {
 
-  map.addSource('seoulhexgrid',{
+  map.addSource("seoulpoints",{
     "type": "geojson",
-    "data": hexgrid
+    "data": "https://github.com/tkahng/mapboxtest1/blob/master/seoulhousingprice%20-%20Copy.geojson"
   })
 
   map.addLayer({
-    id: 'maine',
-    type: 'fill',
-    source: {
-      type: 'geojson',
-      data: seoulhexgrid
-    },
+    id: 'housepoints',
+    type: 'circle',
+    source: "seoulpoints",
     layout: {},
     paint: {'fill-color': '#088', 'fill-opacity': [1]}
   });
